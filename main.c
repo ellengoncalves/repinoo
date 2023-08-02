21c4de75f16528d71fb78bab002e8c1e31367bbd
ellen <ellen.g@aluno.ifsp.edu.br>
2023-08-01 22:07:28 -0300

    primeiro commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void print(); 
+
+int main(int argc, char const *argv[])
+{
+    print();
+    return 0;
+}
+
+void print(){
+    printf("\n\t ** Projeto Complexo!!! ** \n\n");
+}
